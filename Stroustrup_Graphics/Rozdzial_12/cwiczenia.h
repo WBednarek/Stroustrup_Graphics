3d811e78ffc5163261163805bcd3ec15248efbf6
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-04-16 17:49:39 +0200

    - FLTK instalation instruction update - Visual studio configured to wirk with FLTK library and to run programs from  Programming -- Principles and Practice Using C++ (PPP) by Bjarne Stroustrup - Exercise 1 from Stroustrup PPP done.

---
+++ Stroustrup_Graphics/Rozdzial_12/cwiczenia.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+namespace Ex12
+{
+	void cwiczenia();
+
+
+		class cwiczenia
+	{
+	public:
+		cwiczenia();
+		~cwiczenia();
+	};
+
+}
---
+++ Stroustrup_Graphics/mian.cpp
@@ -0,0 +1,16 @@
+//
+// This is example code from Chapter 12.3 "A first example" of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+//
+
+#include "Simple_window.h"    // get access to our window library
+#include "Graph.h"            // get access to our graphics library facilities
+#include "Rozdzial_12\cwiczenia.h"
+
+int main()
+{
+	Ex12::cwiczenia();
+	
+
+	return 0;
+}
