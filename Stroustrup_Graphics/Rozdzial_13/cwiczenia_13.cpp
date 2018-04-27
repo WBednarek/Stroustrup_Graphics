717c029bb63c43672c165738906e669bb5e5e394
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-04-27 02:17:32 +0200

    - Stroustrup Programming: Principles and Practice Using (PPP) Chapter 13 Exercise 5 done. Almost :P as I wanted! I have to make appear red squares instead of empty. - Exercises numbers corrected

--- Stroustrup_Graphics/Rozdzial_13/cwiczenia_13.cpp
+++ Stroustrup_Graphics/Rozdzial_13/cwiczenia_13.cpp
@@ -19,25 +19,27 @@ namespace Ex13
 		//Exercise 1
 		Point t(100, 100);
 		Simple_window win(t, 800, 1000, "Exercises 13");
+		const int grid_size = 8;
 		
 		squares(win);
 
 		//Exercise 2
 		Vector_ref<Rectangle> vecs;
-		int square_size = 100;
+		const int square_size = 100;
 		int count = 0;
-		for (int i = 0; i < 8; ++i)
+		for (int i = 0; i < grid_size; ++i)
 		{
-			for (int j = 0; j < 8; ++j)
+			for (int j = 0; j < grid_size; ++j)
 			{
 				vecs.push_back(new Rectangle(Point(j*square_size, i*square_size), Point((j*square_size) + square_size, (i*square_size) + square_size)));		
 				win.attach(vecs[vecs.size() -1]);
 				
 			}
-			vecs[i*8+i].set_fill_color(Color(Color::red));
+			//Exercise 3
+			vecs[i*grid_size+i].set_fill_color(Color(Color::red));
 		}
 
-		//Exercise 3 - better solution
+		//Exercise 4 - better solution
 		vector<Point> points = { Point(400, 0), Point(100,300), Point(400, 600)};
 	
 		for (int i = 0; i < points.size(); ++i)
@@ -47,7 +49,7 @@ namespace Ex13
 			
 		}
 		/*
-		//Exercise 3 Second solution - worse
+		//Exercise 4 Second solution - worse
 		Image *img1 = new Image(points[0], "200x200.gif");
 		Image img2(points[1], "200x200.gif");
 		Image img3(points[2], "200x200.gif");
@@ -58,7 +60,43 @@ namespace Ex13
 		*/
 		
 
-		win.wait_for_button();
+
+		//Exercise 5
+		while (true)
+		{
+			win.attach(*new Image(Point(0, 0), "fallout.gif"));
+			win.attach(vecs[vecs.size()-1]);
+			vecs[vecs.size() -1].set_fill_color(Color(Color::red));
+			
+			int counter = 1;
+			for (int i = 0; i < grid_size; ++i)
+			{
+				for (int j = 0; j < grid_size; ++j)
+				{
+					
+				    win.attach(*new Image(Point(j*square_size, i*square_size), "fallout.gif"));
+					if (counter > 1)
+					{
+						win.attach(vecs[counter-2]);
+						vecs[counter-2].set_fill_color(Color(Color::red));
+						
+					}
+					++counter;
+					win.wait_for_button();
+					
+				}
+
+			}
+
+
+
+
+
+			
+		}
+
+
+		
 		return 0;
 	}
 
