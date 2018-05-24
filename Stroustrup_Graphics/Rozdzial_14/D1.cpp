63a73d3ea4e7a318479708efceb917185c68b775
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-05-24 02:04:19 +0200

    Special commit with D1 class with a namespace issue. Does not work.

---
+++ Stroustrup_Graphics/Rozdzial_14/D1.cpp
@@ -0,0 +1,27 @@
+#include "D1.h"
+
+// Error can't see the B1 class. No namespace Ex14 on the D1 class.
+void D1::vf()
+{
+}
+
+void D1::f()
+{
+}
+
+
+
+//
+//namespace Ex14
+//{
+//	void D1::vf()
+//	{
+//	}
+//
+//	void D1::f()
+//	{
+//	}
+//}
+
+
+
--- Stroustrup_Graphics/Rozdzial_14/D1.h
+++ Stroustrup_Graphics/Rozdzial_14/D1.h
@@ -3,10 +3,19 @@
 // "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
 #include "../Simple_window.h"    // get access to our window library
 #include "../Graph.h"            // get access to our graphics library facilities
+#include "B1.h"
 
-namespace Ex13
-{
-	int cwiczenia();
-	void squares(Simple_window& win);
 
-}
+	class D1 : B1 // Error can't see the B1 class. No namespace Ex14 on the D1 class.
+	{
+	public:
+		void vf();
+		void f();
+	};
+
+
+	
+
+
+
+
