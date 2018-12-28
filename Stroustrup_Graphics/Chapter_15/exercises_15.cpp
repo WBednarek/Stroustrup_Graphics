9a2134a638df9a8f4856846fe36fdd039867faa9
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-12-28 19:53:28 +0100

    Chapter 15 exercises 2,3,4,5 done.

--- Stroustrup_Graphics/Chapter_15/exercises_15.cpp
+++ Stroustrup_Graphics/Chapter_15/exercises_15.cpp
@@ -6,13 +6,37 @@ using namespace Graph_lib;
 namespace Ex15
 {
 
+	const int xmax = 600;
+	const int ymax = 600;
+
+	const int xoffset = 100;
+	const int yoffset = 100;
+
+	const int xspace = 100;
+	const int yspace = 100;
+
+	const int xlenght = xmax - xoffset - xspace;
+	const int ylenght = ymax - yoffset - yspace;
 
 
+	const int base = 0;
+	const int end = 20;
+	const double xscale = xlenght / (end - base);
+	const double yscale = ylenght / (end - base);
 
 	int exercises()
 	{
 		Point t1(100, 100);
-		Simple_window win(t1, 600, 600, "Wykresy funkcji");
+		Simple_window win(t1, xmax, ymax, "Wykresy funkcji");
+
+		Axis x(Axis::x, Point(xoffset, ymax - yoffset), xlenght, 20, "1 == 20 pixels");
+		Axis y(Axis::y, Point(xoffset, ymax - yoffset), ylenght, 20, "1 == 20 pixels");
+
+		x.set_color(Color::red);
+		y.set_color(Color::red);
+
+		win.attach(x);
+		win.attach(y);
 		win.wait_for_button();
 
 
