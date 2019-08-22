845f0f003707ab27a2c7db37daf477a886a032bc
Domowy <Domowy@DESKTOP-UGN94MI>
2019-08-23 00:19:18 +0200

    Compiled fltk library and tutorial of running fltk libraries with Visual Studio 2019 Community added.

--- Stroustrup_Graphics/Rozdzial_14/D21.h
+++ Stroustrup_Graphics/Rozdzial_14/D21.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "C:\Users\Domowy\Documents\Visual Studio 2015\Projects\PROGRAMOWANIE STROUSTRUP\Rozdzia³ 4\Stroustrup_Graphics\Stroustrup_Graphics\Rozdzial_14\B2.h"
+#include "../Rozdzial_14/B2.h"
 #include "../Simple_window.h"
 #include "../Graph.h"
 
