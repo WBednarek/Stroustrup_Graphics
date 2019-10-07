7f3ef354381388ca6ecf94196d9088e51297d45d
Wiktor Bednarek <wdbednarek@quinnipiac.edu>
2019-10-07 23:01:17 +0200

    Chapter 15, exercise 4 done. Little refactorization also was done.

--- Stroustrup_Graphics/Chapter_16/exercises_16.cpp
+++ Stroustrup_Graphics/Chapter_16/exercises_16.cpp
@@ -13,8 +13,10 @@ namespace Ex16
 		next_x(Point(x_max() - 310, 0), 50, 20, "x:"),
 		next_y(Point(x_max() - 210, 0), 50, 20, "y:"),
 		xy_out(Point(100,0), 100, 20, "bierz¹cy punkt:"),
-		color_menu(Point(x_max() - 70, 30), 70, 20,Menu::vertical, "Kolor" ),
-		menu_button(Point(x_max() - 80, 30), 80, 20, "Menu kolorów", cb_menu)
+		color_menu(Point(x_max() - 70, button_height), 70, 20,Menu::vertical, "Kolor" ),
+		color_menu_button(Point(x_max() - 90, button_height), 80, 20, "Menu kolorów", cb_color_menu),
+		style_menu(Point(x_max() - 70, button_height*4), 70, 20, Menu::vertical, "Style"),
+		style_menu_button(Point(x_max() - 90, button_height * 4), 80, 20, "Menu stylów", cb_style_menu)
 	{
 		attach(next_button);
 		attach(quit_buttton);
@@ -25,9 +27,16 @@ namespace Ex16
 		color_menu.attach(new Button(Point(0, 0), 0, 0, "czerwony", cb_red));
 		color_menu.attach(new Button(Point(0, 0), 0, 0, "niebieski", cb_blue));
 		color_menu.attach(new Button(Point(0, 0), 0, 0, "czarny", cb_black));
+		style_menu.attach(new Button(Point(0, 0), 0, 0, "solid", cb_solid));
+		style_menu.attach(new Button(Point(0, 0), 0, 0, "dash", cb_dash));
+		style_menu.attach(new Button(Point(0, 0), 0, 0, "dashdot", cb_dashdot));
+		style_menu.attach(new Button(Point(0, 0), 0, 0, "dashdotdot", cb_dashdotdot));
 		attach(color_menu);
+		attach(style_menu);
 		color_menu.hide();
-		attach(menu_button);
+		style_menu.hide();
+		attach(color_menu_button);
+		attach(style_menu_button);
 		attach(lines);
 		
 	}
@@ -60,28 +69,57 @@ namespace Ex16
 	void Lines_window::cb_red(Address, Address pw)
 	{
 		reference_to<Lines_window>(pw).red_pressed();
-		reference_to<Lines_window>(pw).hide_menu();
+		reference_to<Lines_window>(pw).hide_color_menu();
 	}
 
 	void Lines_window::cb_blue(Address, Address pw)
 	{
 		reference_to<Lines_window>(pw).blue_pressed();
-		reference_to<Lines_window>(pw).hide_menu();
+		reference_to<Lines_window>(pw).hide_color_menu();
 
 	}
 
 	void Lines_window::cb_black(Address, Address pw)
 	{
 		reference_to<Lines_window>(pw).black_pressed();
-		reference_to<Lines_window>(pw).hide_menu();
+		reference_to<Lines_window>(pw).hide_color_menu();
 	
 	}
 
-	void Lines_window::cb_menu(Address, Address pw)
+	void Lines_window::cb_color_menu(Address, Address pw)
 	{
-		reference_to<Lines_window>(pw).menu_pressed();
+		reference_to<Lines_window>(pw).color_menu_pressed();
 	}
 
+	void Lines_window::cb_solid(Address, Address pw)
+	{
+		reference_to<Lines_window>(pw).solid_pressed();
+		reference_to<Lines_window>(pw).hide_style_menu();
+	}
+
+	void Lines_window::cb_dash(Address, Address pw)
+	{
+		reference_to<Lines_window>(pw).dash_pressed();
+		reference_to<Lines_window>(pw).hide_style_menu();
+	}
+
+	void Lines_window::cb_dashdot(Address, Address pw)
+	{
+		reference_to<Lines_window>(pw).dashdot_pressed();
+		reference_to<Lines_window>(pw).hide_style_menu();
+	}
+
+	void Lines_window::cb_dashdotdot(Address, Address pw)
+	{
+		reference_to<Lines_window>(pw).dashdotdot_pressed();
+		reference_to<Lines_window>(pw).hide_style_menu();
+	}
+
+
+	void Lines_window::cb_style_menu(Address, Address pw)
+	{
+		reference_to<Lines_window>(pw).style_menu_pressed();
+	}
 
 
 
--- Stroustrup_Graphics/Chapter_16/exercises_16.h
+++ Stroustrup_Graphics/Chapter_16/exercises_16.h
@@ -11,39 +11,56 @@ namespace Ex16
 		Lines_window(Point xy, int w, int h, const string& title);
 		Open_polyline lines;
 	private:
+		const int button_height = 30;
 		Button next_button;
 		Button quit_buttton;
 		In_box next_x;
 		In_box next_y;
 		Out_box xy_out;
 		Menu color_menu;
-		Button menu_button;
+		Button color_menu_button;
+		Menu style_menu;
+		Button style_menu_button;
+	
 
 		void change(Color c) { lines.set_color(c); }
-		void hide_menu() { color_menu.hide(); menu_button.show(); }
+		void change_style(Line_style s) { lines.set_style(s); }
+		void hide_color_menu() { color_menu.hide(); color_menu_button.show(); }
+		void hide_style_menu() { style_menu.hide(); style_menu_button.show(); }
 
 		void next();
 		void quit();
 		void red_pressed() { change(Color::red); }
 		void blue_pressed() { change(Color::blue); }
 		void black_pressed() { change(Color::black); }
-		void menu_pressed() { menu_button.hide(); color_menu.show(); }
+		void color_menu_pressed() { color_menu_button.hide(); color_menu.show(); }
 
+		void solid_pressed() { change_style(Line_style::solid); }
+		void dash_pressed() { change_style(Line_style::dash); }
+		void dashdot_pressed() { change_style(Line_style::dashdot); }
+		void dashdotdot_pressed() { change_style(Line_style::dashdotdot); }
+		void style_menu_pressed() { style_menu_button.hide(); style_menu.show(); }
+
+
+		
 
 		static void cb_next(Address, Address);
 		static void cb_quit(Address, Address);
 		static void cb_red(Address, Address);
 		static void cb_blue(Address, Address);
 		static void cb_black(Address, Address);
-		static void cb_menu(Address, Address);
+		static void cb_color_menu(Address, Address);
+
+		static void cb_solid(Address, Address);
+		static void cb_dash(Address, Address);
+		static void cb_dashdot(Address, Address);
+		static void cb_dashdotdot(Address, Address);
+		static void cb_style_menu(Address, Address);
 		
 
 	};
 	
 	
-	void exercises();
-
-
 
 
 
